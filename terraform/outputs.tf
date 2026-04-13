12d80e136288b068add445d8be06237600f2b4ff
ngangmin <153655605+ngangmin@users.noreply.github.com>
2026-04-12 21:36:14 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,14 @@ output "public_subnets" {
 output "private_subnets" {
   value = module.vpc.private_subnets
 }
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
