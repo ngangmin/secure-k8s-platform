d52a778bdfecf7ae2d62fd56f6f3d09156c96fe9
ngangmin <153655605+ngangmin@users.noreply.github.com>
2026-04-12 20:57:10 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "eks-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "eks-vpc"
+    Environment = "dev"
+  }
+}
